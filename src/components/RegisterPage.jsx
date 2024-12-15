174aaa0bcf36f8b350655ddc77262b40c6030d9e
Roshan Kumar Badola <roshanbadola06@gmail.com>
2024-12-15 21:17:42 +0530

    push

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -46,7 +46,7 @@ const RegisterPage = () => {
                 photoBase64: values.photo,
             }
             console.log(payload)
-            const response = await fetch("http://localhost:3000/user/", {
+            const response = await fetch("https://magical-faun-f73683.netlify.app/user/", {
                 method: "POST",
                 body: JSON.stringify(payload),
                 headers: {
